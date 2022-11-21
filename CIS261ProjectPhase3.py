a5ab398804902ce7e69614169a30030c49dbd8d9
Zakk <86750066+Z4KKD@users.noreply.github.com>
2022-11-21 18:12:10 -0500

    Update CIS261ProjectPhase3.py

--- CIS261ProjectPhase3.py
+++ CIS261ProjectPhase3.py
@@ -99,7 +99,7 @@ def PrintTotals(EmpTotals):
 
 if __name__ == "__main__":
     # write the line of code to open a file Employees.txt in append mode and assign it to EmpFile
-
+    EmpFile = ("Employees.txt", "a")
     #EmpDetailList = []
     EmpTotals = {}
     DetailsPrinted = False
